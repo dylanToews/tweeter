907a8b7bacc326635fcce3031bc2ccff5e8fad36
dylanToews <dylan.toews@yahoo.ca>
2022-11-08 13:13:52 -0700

    counter value connected to text length in textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  // --- our code goes here ---\
 
-    $('#tweet-text').on('keyup', function(event) {
-      let content = $("#tweet-text").val()
-      console.log(content)
-      console.log(content.length)
-    })
-   
 
-console.log("something happened")
+  // const tweetForm = $('.new-tweet');
+  // const tweetText = tweetForm.find('textarea');
+  // const counter = tweetForm.find('.counter');
+  // console.log(counter.defaultValue);
+
+  $('#tweet-text').on('keyup', function(event) {
+    let content = event.target.value;
+    console.log(content);
+    console.log(content.length);
+    $('.counter').val(140 - content.length)  
+    
+  });
+
+
+  console.log("something happened");
 
 });
 
