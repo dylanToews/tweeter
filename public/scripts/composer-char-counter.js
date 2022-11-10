1585bfd0d3376778edef17f2d84f8e2aa47ff176
dylanToews <dylan.toews@yahoo.ca>
2022-11-10 14:20:15 -0700

    Refactoring of code throughout

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
 
+
+  // Character counter function
+  
   $('#tweet-text').on('keyup', function(event) {
     let content = event.target.value;
-
     $('.counter').val(140 - content.length);
-
     if ($('.counter').val() < 0) {
       $('.counter').css("color", "red");
     }
     if ($('.counter').val() > 0) {
       $('.counter').css("color", "rgb(182, 176, 167)");
     }
-  
-    
   });
-
 });
 
